f37cf8f2ee5d4352b1bfb09da4cc65b7b1cccd2a
Yahya Houaoui <yahiahwewi@gmail.com>
2022-12-04 18:49:40 +0100

    Install and setup apollo client

--- BooksMobile/App.tsx
+++ BooksMobile/App.tsx
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
+import { ApolloClient ,InMemoryCache ,ApolloProvider} from '@apollo/client/core';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+const API_KEY = "williston::stepzen.net+1000::b755a7150e0e3a92fe0aba992d3c55673e3ac5fff8b3cb054c3169d3c804294f";
+
+const client = new ApolloClient({
+uri: "https://williston.stepzen.net/api/oily-snake/__graphql",
+ headers:{
+Authorization :'ApiKey ${}',
+
+
+},
+cache: new InMemoryCache(),
+
+});
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
@@ -14,7 +26,11 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+        <ApolloProvider client ={client}>
+
+
+        </ApolloProvider>
+        {/* <Navigation colorScheme={colorScheme} /> */}
         <StatusBar />
       </SafeAreaProvider>
     );
